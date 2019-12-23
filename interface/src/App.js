7a9951802b18b1c7cd52901826f8421904061608
ionutc3 <ionutmihoci@yahoo.com>
2019-12-23 16:06:07 +0200

    added login component

--- interface/src/App.js
+++ interface/src/App.js
@@ -1,34 +1,37 @@
-import React from "react";
+import React from 'react';
 
-import { Route, Switch, withRouter, Redirect } from "react-router-dom";
+import { Route, Switch, withRouter, Redirect } from 'react-router-dom';
 
-import "./App.css";
-import LandingPage from "./components/LandingPageComponent/LandingPageComponent";
-import QuizzesPage from "./components/QuizzesPage/QuizzesPageComponent";
-import MenuComponent from "./components/MenuComponent/MenuComponent";
+import './App.css';
+import LandingPage from './components/LandingPageComponent/LandingPageComponent';
+import QuizzesPage from './components/QuizzesPage/QuizzesPageComponent';
+import MenuComponent from './components/MenuComponent/MenuComponent';
+import LoginComponent from './components/LoginComponent/LoginComponent';
 
-import { Provider as AppProvider } from "./Contexts/AppContext/index";
-import { Provider as QuizProvider } from "./Contexts/QuizPageContext/index";
-import QuizPage from "./components/QuizPageComponent/QuizPageComponent";
+import { Provider as AppProvider } from './Contexts/AppContext/index';
+import { Provider as QuizProvider } from './Contexts/QuizPageContext/index';
+import QuizPage from './components/QuizPageComponent/QuizPageComponent';
 
 const App = () => {
   return (
     <AppProvider>
-      {window.location.pathname !== "/home" && <MenuComponent />}
+      {window.location.pathname !== '/home' && <MenuComponent />}
 
       <Switch>
-        <Route exact path="/">
-          <Redirect to="/home" />
+        <Route exact path='/'>
+          <Redirect to='/home' />
         </Route>
 
-        <Route path="/home" component={LandingPage} />
+        <Route path='/home' component={LandingPage} />
 
-        <Route path="/create-quiz" />
+        <Route path='/login' component={LoginComponent} />
+
+        <Route path='/create-quiz' />
 
         <QuizProvider>
-          <Route path="/solve-quiz" component={QuizzesPage} />
+          <Route path='/solve-quiz' component={QuizzesPage} />
 
-          <Route path="/quiz" component={QuizPage} />
+          <Route path='/quiz' component={QuizPage} />
         </QuizProvider>
       </Switch>
     </AppProvider>
