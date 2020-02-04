f4b2b63cf51b5fdbd608bd230228a014abab6aef
iQuestUniversitymic <ionutmihoci@yahoo.com>
2020-02-04 20:28:19 +0200

    little details

--- interface/src/components/MenuComponent/MenuComponent.js
+++ interface/src/components/MenuComponent/MenuComponent.js
@@ -8,70 +8,72 @@ import { Context as ApplicationContext } from '../../Contexts/AppContext';
 import { Context as QuizContext } from '../../Contexts/QuizPageContext';
 
 const MenuComponent = () => {
-  const appContext = useContext(ApplicationContext);
-  const quizContext = useContext(QuizContext);
+	const appContext = useContext(ApplicationContext);
+	const quizContext = useContext(QuizContext);
 
-  const { history } = appContext;
-  const { setQuizQuestions } = quizContext;
+	const { history } = appContext;
+	const { setQuizQuestions } = quizContext;
 
-  const [activeItem, setActiveItem] = useState('');
+	const [ activeItem, setActiveItem ] = useState('');
 
-  return (
-    <Menu inverted fixed='top' borderless>
-      <Link to='/home'>
-        <Menu.Item name='Home' color='green'>
-          <img src={logo} alt='logo' />
-        </Menu.Item>
-      </Link>
-      {sessionStorage.getItem('username') && (
-        <Menu.Menu position='right'>
-          <Menu.Item
-            name='create quiz'
-            active={activeItem === 'create quiz'}
-            onClick={(e, { name }) => {
-              setActiveItem(name);
-              history.push('/create-quiz');
-            }}
-          />
-          <Menu.Item
-            name='solve quiz'
-            active={activeItem === 'solve quiz'}
-            onClick={(e, { name }) => {
-              setActiveItem(name);
-              history.push('/solve-quiz');
-            }}
-          />
-          <Menu.Item
-            name='my quizzes'
-            active={activeItem === 'my quizzes'}
-            onClick={(e, { name }) => {
-              setActiveItem(name);
-              history.push('/user-quizzes');
-            }}
-          />
-          <Menu.Item
-            name='logout'
-            active={activeItem === 'logout'}
-            onClick={(e, { name }) => {
-              setActiveItem(name);
-              sessionStorage.clear();
-              history.push('/login');
-            }}
-          />
-          <Menu.Item
-            name='user-profile'
-            active={activeItem === 'user-profile'}
-            onClick={(e, { name }) => {
-              setActiveItem(name);
-              history.push('/update-profile');
-            }}
-          >
-            <Icon name='user outline' />
-          </Menu.Item>
-        </Menu.Menu>
-      )}
-    </Menu>
-  );
+	return (
+		<Menu inverted fixed="top" borderless>
+			<Link to="/home">
+				<Menu.Item name="Home" color="green">
+					<img src={logo} alt="logo" />
+				</Menu.Item>
+			</Link>
+			{sessionStorage.getItem('username') && (
+				<Menu.Menu position="right">
+					<Menu.Item
+						name="create quiz"
+						active={activeItem === 'create quiz'}
+						onClick={(e, { name }) => {
+							setQuizQuestions([]);
+							setActiveItem(name);
+							history.push('/create-quiz');
+						}}
+					/>
+					<Menu.Item
+						name="solve quiz"
+						active={activeItem === 'solve quiz'}
+						onClick={(e, { name }) => {
+							setActiveItem(name);
+							history.push('/solve-quiz');
+						}}
+					/>
+					<Menu.Item
+						name="my quizzes"
+						active={activeItem === 'my quizzes'}
+						onClick={(e, { name }) => {
+							setQuizQuestions([]);
+							setActiveItem(name);
+							history.push('/user-quizzes');
+						}}
+					/>
+					<Menu.Item
+						name="logout"
+						active={activeItem === 'logout'}
+						onClick={(e, { name }) => {
+							setActiveItem(name);
+							sessionStorage.clear();
+							history.push('/login');
+						}}
+					/>
+					<Menu.Item
+						name="user-profile"
+						active={activeItem === 'user-profile'}
+						onClick={(e, { name }) => {
+							setActiveItem(name);
+							history.push('/update-profile');
+						}}
+					>
+						<Icon name="user outline" />
+					</Menu.Item>
+				</Menu.Menu>
+			)}
+		</Menu>
+	);
 };
 
 export default MenuComponent;
