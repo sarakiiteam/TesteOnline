1887e4ba3423cd1b79f1597ba5ab767cb323a676
iQuestUniversitymic <ionutmihoci@yahoo.com>
2020-02-04 19:12:29 +0200

    cucu

--- interface/src/components/UserDashboardPage/UserQuizzesDashboardPage.js
+++ interface/src/components/UserDashboardPage/UserQuizzesDashboardPage.js
@@ -7,48 +7,48 @@ import './Dashboard.css';
 import UserQuizCard from './UserQuizCardComponent';
 
 const UserQuizzes = () => {
-  const [quizzes, setQuizzes] = useState([]);
+	const [ quizzes, setQuizzes ] = useState([]);
 
-  useEffect(() => {
-    fetch(
-      `http://localhost:8080/api/tests/users/,
+	useEffect(() => {
+		fetch(
+			`http://localhost:8080/api/tests/users/
       ${sessionStorage.getItem('username')}`,
-      {
-        method: 'GET',
-        headers: {
-          'Content-Type': 'application/json'
-        }
-      }
-    )
-      .then(data => data.json())
-      .then(parsedData => {
-        console.log(parsedData);
-        setQuizzes(parsedData['tests']);
-      })
-      .catch(error => console.log(error));
-  }, []);
+			{
+				method: 'GET',
+				headers: {
+					'Content-Type': 'application/json'
+				}
+			}
+		)
+			.then((data) => data.json())
+			.then((parsedData) => {
+				console.log(parsedData);
+				setQuizzes(parsedData['tests']);
+			})
+			.catch((error) => console.log(error));
+	}, []);
 
-  return (
-    <WrapperComponent>
-      <Card.Group itemsPerRow={3} centered stackable className='quizzesStyle'>
-        {quizzes ? (
-          quizzes.map((quiz, index) => (
-            <UserQuizCard
-              key={index}
-              name={quiz.testName}
-              difficulty={quiz.testDifficulty}
-              description={quiz.description}
-            />
-          ))
-        ) : (
-          <Loader active inline='centered'>
-            Fetching your quizzes..
-          </Loader>
-        )}
-      </Card.Group>
-      <Divider />
-    </WrapperComponent>
-  );
+	return (
+		<WrapperComponent>
+			<Card.Group itemsPerRow={3} centered stackable className="quizzesStyle">
+				{quizzes ? (
+					quizzes.map((quiz, index) => (
+						<UserQuizCard
+							key={index}
+							name={quiz.testName}
+							difficulty={quiz.testDifficulty}
+							description={quiz.description}
+						/>
+					))
+				) : (
+					<Loader active inline="centered">
+						Fetching your quizzes..
+					</Loader>
+				)}
+			</Card.Group>
+			<Divider />
+		</WrapperComponent>
+	);
 };
 
 export default UserQuizzes;
--- server/src/main/java/controllers/TestController.java
+++ server/src/main/java/controllers/TestController.java
@@ -156,6 +156,15 @@ public class TestController {
 
         // filter the result
         final Map<String, List<Test>> userTests = new HashMap<>();
+        final List<Test> listx = testService.getFilteredTests(
+                test -> test
+                        .getUser()
+                        .getUsername()
+                        .equals(
+                                username.toLowerCase()
+                        )
+        ).stream().peek(x -> x.setUser(null)).collect(Collectors.toList());
+
         userTests.put(
                 "tests",
                 testService.getFilteredTests(
